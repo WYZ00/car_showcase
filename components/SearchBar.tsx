dc99cfb1567a2d2a79080ca18a541ae2728c54b2
WYZ00 <thanthtettin@gmail.com>
2023-06-10 18:14:55 +0630

    Car Catalogue add

---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState } from "react";
+import { SearchManufacturer } from "./";
+
+const SearchBar = () => {
+  const [Manufacturer, setManufacturer] = useState("");
+
+  const handleSearch = () => {};
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer 
+        manufacturer={Manufacturer}
+        setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomerFilter from "./CustomerFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export { Hero, CustomButton, Navbar, Footer,SearchBar,CustomerFilter,SearchManufacturer };
